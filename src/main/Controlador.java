1d7bc0f55c1d641bc38b55eeb05fd531717b3de2
rusafe <150953396+rusafe@users.noreply.github.com>
2024-10-08 19:50:34 +0200

    finished word finder and word replacer

--- src/main/Controlador.java
+++ src/main/Controlador.java
@@ -48,5 +48,29 @@ public class Controlador {
 				vista.getTxaEstructuraDirectori().setText(model.obtainEstructuraDirectori(directori, paraulaBuscar, respetarMayuscules, respetarAcentos));
 			}
 		});
+		vista.getBtnReemplazar().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(vista.getTxtIntroduixDirectori().getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "No se ha introduit directori.", "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				if(vista.getTxtBuscar().getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "No se ha introduit res per a buscar.", "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				if(vista.getTxtReemplazar().getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "No se ha introduit res per a reemplazar.", "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
+				File directori = new File(vista.getTxtIntroduixDirectori().getText());
+				String paraulaReemplazar = vista.getTxtBuscar().getText();
+				String paraulaNova = vista.getTxtReemplazar().getText();
+				boolean respetarMayuscules = vista.getChkRespetarMayuscules().isSelected();
+				boolean respetarAcentos = vista.getChkRespetarAcentos().isSelected();
+				
+				vista.getTxaEstructuraDirectori().setText(model.obtainEstructuraDirectori(directori, paraulaReemplazar, paraulaNova, respetarMayuscules, respetarAcentos));
+			}
+		});
 	}
 }
--- src/main/Model.java
+++ src/main/Model.java
@@ -1,23 +1,26 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.text.Normalizer;
 import java.text.Normalizer.Form;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Model {
-	ArrayList<File> fichers = new ArrayList<File>();
-	
 	public String obtainEstructuraDirectori(File directori) {
 		return String.format("%s%s", directori.getName(), estructuraDirectori(directori, 0));
 	}
 	
 	public String obtainEstructuraDirectori(File directori, String paraula, boolean respetarMayuscules, boolean respetarAcentos) {
 		return String.format("%s%s", directori.getName(), estructuraDirectori(directori, 0, paraula, respetarMayuscules, respetarAcentos));
+	}
+	
+	public String obtainEstructuraDirectori(File directori, String paraulaReemplazar, String paraulaNova, boolean respetarMayuscules, boolean respetarAcentos) {
+		return String.format("%s%s", directori.getName(), estructuraDirectori(directori, 0, paraulaReemplazar, paraulaNova, respetarMayuscules, respetarAcentos));
 	}
 	
 	private String estructuraDirectori(File directori, int profunditat) {
@@ -38,7 +41,6 @@ public class Model {
 				}
 			}
 			else {
-				fichers.add(archiu);
 				estructura += String.format("%s%s %s (%.1f KB - %s)", System.lineSeparator(), anyadirSeparacioRuta(profunditat), archiu.getName(), transformarAKilobytes(archiu.length()), formatoFecha.format(new Date()));
 			}
 		}
@@ -63,8 +65,31 @@ public class Model {
 				}
 			}
 			else {
-				fichers.add(archiu);
 				estructura += String.format("%s%s %s (%d coincidències)", System.lineSeparator(), anyadirSeparacioRuta(profunditat), archiu.getName(), cantitatCoincidencies(archiu, paraula, respetarMayuscules, respetarAcentos));
+			}
+		}
+		
+		return estructura;
+	}
+	
+	private String estructuraDirectori(File directori, int profunditat, String paraulaReemplazar, String paraulaNova, boolean respetarMayuscules, boolean respetarAcentos) {
+		String estructura = "";
+		
+		File[] continguts = directori.listFiles();
+		
+		if(continguts.length == 0) {
+			return "";
+		}
+		
+		for(File archiu : continguts) {
+			if(archiu.isDirectory()) {
+				if(archiu.list().length != 0) {
+					estructura += String.format("%s%s\\%s", System.lineSeparator(), anyadirSeparacioRuta(profunditat), archiu.getName());
+					estructura += estructuraDirectori(archiu, profunditat + 1, paraulaReemplazar, paraulaNova, respetarMayuscules, respetarAcentos);					
+				}
+			}
+			else {
+				estructura += String.format("%s%s %s (%d reemplazos)", System.lineSeparator(), anyadirSeparacioRuta(profunditat), archiu.getName(), reemplazar(archiu, paraulaReemplazar, paraulaNova, respetarMayuscules, respetarAcentos));
 			}
 		}
 		
@@ -89,24 +114,31 @@ public class Model {
 	
 	private int cantitatCoincidencies(File archiu, String paraula, boolean respetarMayuscules, boolean respetarAcentos) {
 		int coincidencies = 0;
+		String text;
 		
-		String text = llegirArchiu(archiu);
 		
-		if(!respetarMayuscules)
+		try {
+			text = llegirArchiu(archiu);
+		} catch (Exception e) {
+			return 0;
+		}
+		
+		if(!respetarMayuscules) {
 			paraula = paraula.toLowerCase();
-		if(!respetarAcentos)
+			text = text.toLowerCase();
+		}
+		if(!respetarAcentos) {
 			paraula = llevarAcento(paraula);
+			text = llevarAcento(text);
+		}
 		
 		int indicePosibleCoincidencia = text.indexOf(paraula.charAt(0));
 		String posibleCoincidencia;
 		while(indicePosibleCoincidencia != -1) {
+			if(indicePosibleCoincidencia + paraula.length() > text.length())
+				break;
+			
 			posibleCoincidencia = text.substring(indicePosibleCoincidencia, indicePosibleCoincidencia + paraula.length());
-
-			if(!respetarMayuscules)
-				posibleCoincidencia = posibleCoincidencia.toLowerCase();
-			if(!respetarAcentos)
-				posibleCoincidencia = llevarAcento(posibleCoincidencia);
-				
 			
 			if(paraula.equals(posibleCoincidencia))
 				coincidencies++;
@@ -117,10 +149,52 @@ public class Model {
 		return coincidencies;
 	}
 	
-	private String llegirArchiu(File archiu) {
+	private int reemplazar(File archiu, String paraulaReemplazar, String paraulaNova, boolean respetarMayuscules, boolean respetarAcentos) {
+		int cantitatReemplazos = cantitatCoincidencies(archiu, paraulaReemplazar, respetarMayuscules, respetarAcentos);
+
+		if(cantitatReemplazos == 0)
+			return 0;
+		
+		String ruta = archiu.getParentFile().getPath();
+		String nouNom = String.format("MOD_%s", archiu.getName());
+		
+		String text; 
+		
+		try {
+			text = llegirArchiu(archiu);			
+		} catch (Exception e) {
+			return 0;
+		}
+		
+		if(!respetarMayuscules) {
+			paraulaReemplazar = paraulaReemplazar.toLowerCase();
+			text = text.toLowerCase();
+		}
+		if(!respetarAcentos) {
+			paraulaReemplazar = llevarAcento(paraulaReemplazar);
+			text = llevarAcento(text);
+		}
+		
+		String textReemplazat = text.replaceAll(paraulaReemplazar, paraulaNova);
+
+		try {
+			FileWriter fw = new FileWriter(String.format("%s%s%s", ruta, File.separator, nouNom));	
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			bw.write(textReemplazat);
+			
+			bw.close();
+			fw.close();
+		} catch (Exception e) {
+			return 0;
+		}
+
+		return cantitatReemplazos;
+	}
+	
+	private String llegirArchiu(File archiu) throws Exception {
 		String text = "";
 		try {
-			
 			FileReader fr = new FileReader(archiu);
 			BufferedReader br = new BufferedReader(fr);
 			
@@ -129,13 +203,13 @@ public class Model {
 				text += linea;
 				linea = br.readLine();
 				if(linea != null)
-					text += "\n";
+					text += System.lineSeparator();
 			}
 			
 			br.close();
 			fr.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw e;
 		}
 		return text;
 	}
